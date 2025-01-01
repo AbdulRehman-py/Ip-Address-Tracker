ed5dde40c8d0dffc1fda138e373d94b4f945ec70
abdul <taylorkings30@gmail.com>
2025-01-01 15:19:06 +0500

    header part is done down to data display part

---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+const Nav = ({buttonIcon}) => {
+  return (
+    <nav id="nav">
+      <h1 className="heading">Ip Address Tracker</h1>
+      <div className="input-button">
+        <form action="submit">
+          <input  type="text" placeholder="Search for any IP address or domain" />
+        </form>
+        <button type="submit">{buttonIcon}</button>
+        </div>
+      
+    </nav>
+  );
+};
+
+export default Nav;
